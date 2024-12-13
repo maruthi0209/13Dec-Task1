de306856c7aea3def8e633be21f759075066915a
maruthi0209 <sethumaruthi93@gmail.com>
2024-12-13 21:01:21 +0530

    Initial commit for Task 1

---
+++ script.js
@@ -0,0 +1,181 @@
+// Question 1
+/**
+ * function sayHello() { 
+console.log(“Hello”); 
+}
+ */
+// First Class Function
+var myVar = function sayHello() {
+    console.log("Hello");
+}
+// Anonymous Function
+var sayHello = function () {
+    console.log("Hello");
+}
+
+
+// Question 2
+/**
+ * function printNumber() { 
+console.log(5); 
+}
+ */
+// First Class Function
+var myVar = function printNumber() {
+    console.log(5);
+}
+// Anonymous Function
+var printNumber = function () {
+    console.log(5);
+}
+
+
+// Question 3
+/**
+ * function addAndPrint() { 
+console.log(2 + 3); 
+}
+ */
+// First Class Function
+var myVar = function addAndPrint() {
+    console.log(2 + 3);
+}
+// Anonymous Function 
+var addAndPrint = function () {
+    console.log(2 + 3);
+}
+
+
+// Question 4
+/**
+ * function add(a, b) {
+ 	return a + b; 
+}
+ */
+// First Class Function 
+var myVar = function add(a , b) {
+    return a + b;
+}
+// Anonymous Function
+var add = function() {
+    return a + b;
+}
+
+
+// Question 5
+/**
+ * function subtract(a, b) { 
+return a - b; 
+}
+ */
+// First Class Function
+var myVar = function subtract(a , b) {
+    return a - b;
+}
+// Anonymous Function
+var subtract = function () {
+    return a - b;
+}
+
+
+// Question 6
+/**
+ * function double(x) { 
+return x + x; 
+}
+ */
+// First Class Function
+var myVar = function double(x) {
+    return x + x;
+}
+// Anonymous Function
+var double = function () {
+    return x + x;
+}
+
+
+// Question 7
+/**
+ * function square(x) { 
+return x * x; 
+}
+ */
+// First Class Function
+var myVar = function square(x) {
+    return x * x;
+}
+// Anonymous Function
+var square = function () {
+    return x * x;
+}
+
+
+// Question 8
+/**
+ * function isPositive(x) { 
+return x > 0;
+}
+ */
+// First Class Function
+var myVar = function isPositive(x) {
+    return x > 0;
+}
+// Anonymous Function
+var isPositive = function () {
+    return x > 0;
+}
+
+
+// Question 9
+/**
+ * function absolute(x) { 
+if (x < 0) { 
+return 0 - x; 
+}
+ return x; 
+}
+ */
+// First Class Function
+var myVar = function absolute(x) {
+    if (x < 0) { 
+        return 0 - x; 
+    }
+    else {
+        return x; 
+    }
+}
+// Anonymous Function
+var absolute = function () {
+    if (x < 0) {
+        return 0 - x;
+    } else {
+        return x;
+    }
+}
+
+
+// Question 10
+/**
+ * function max(a, b) { 
+if (a > b) { 
+return a;
+} 
+return b;
+ }
+ */
+// First Class Function
+var myVar = function max(a , b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+// Anonymous Function
+var max = function (a , b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
